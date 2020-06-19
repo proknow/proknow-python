533094a7edc3e88190a55047f875a59ac4739193
Kyle Burnett <kyle.burnett@elekta.com>
2020-06-19 06:46:08 -0400

    [ch6142] Failed connections should be retried

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="proknow",
-    version="0.16.1",
+    version="0.16.2",
     author="ProKnow",
     author_email="support@proknow.com",
     description="Python library for the ProKnow API.",
